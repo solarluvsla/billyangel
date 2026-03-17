27cb756d62ecf2e72ffbdeef8a4116a6f7964cb4
solarluv <solarluvsla@gamil.com>
2026-03-17 20:25:21 +0900

    haeader 재수정

--- js/common.js
+++ js/common.js
@@ -1,16 +1,19 @@
 $(function(){
   //네비게이션
-  $('nav ul.gnb>li').hover(function(){
-    $('ul.sub').stop().slideDown();
-  }, function(){
-    $('ul.sub').stop().slideUp();
-  });
+$('nav ul.gnb').hover(
+    function() {
+      $('ul.sub, .headerDim').stop().slideDown();
+    }, 
+    function() {
+      $('ul.sub, .headerDim').stop().slideUp();
+    }
+  );
 
   //main_visual
   $('.main_visual ul li:gt(0)').hide();
   setInterval(function () {
     $('.main_visual ul li').first().fadeOut().next().fadeIn().end().appendTo('.main_visual ul');
-  }, 3000)
+  },16000)
 
 
 });
